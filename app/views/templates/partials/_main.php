3f4ec1d9905582a2c54af90c169a962602ebb9e4
Gouvaerts jordan <pro.jordang@gmail.com>
2022-05-12 13:35:55 +0200

    Decoupe du template en partials

---
+++ app/views/templates/partials/_main.php
@@ -0,0 +1,14 @@
+<!-- MAIN -->
+<section class="ftco-section ftco-degree-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 ftco-animate">
+                <div class="container">
+                    <?php echo $content; ?>
+                </div>
+            </div>
+            <!-- aside -->
+            <?php include "../app/views/templates/partials/_aside.php";?>
+        </div>
+    </div>
+</section> 
